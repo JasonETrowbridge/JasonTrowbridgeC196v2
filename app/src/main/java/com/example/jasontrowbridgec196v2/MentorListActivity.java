d29d806c47e0a1170ebed60c2b5a290bd8065076
JasonETrowbridge <jakevsnake@wraithrev.com>
2019-12-20 14:24:11 -0500

    Added Mentor xml's, assessment xmls, and note xml files.

---
+++ app/src/main/java/com/example/jasontrowbridgec196v2/MentorListActivity.java
@@ -0,0 +1,14 @@
+package com.example.jasontrowbridgec196v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MentorListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor_list);
+    }
+}
---
+++ app/src/main/java/com/example/jasontrowbridgec196v2/NoteEditorActivity.java
@@ -0,0 +1,14 @@
+package com.example.jasontrowbridgec196v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+    }
+}
